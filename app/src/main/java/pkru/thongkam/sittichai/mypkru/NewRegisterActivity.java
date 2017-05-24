159cb195cdda8129c0907226a15acc0c1a840fdd
cctvAndwifimap <peem1606@gmail.com>
2017-05-24 22:00:30 +0700

    finish 24May

--- app/src/main/java/pkru/thongkam/sittichai/mypkru/NewRegisterActivity.java
+++ app/src/main/java/pkru/thongkam/sittichai/mypkru/NewRegisterActivity.java
@@ -5,9 +5,9 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,14 +17,11 @@ import android.widget.ImageView;
 public class NewRegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
-
     private EditText nameEditText, userEditText, passwordEditText;
     private ImageView backImageView, humanImageView, cameraImageView;
     private Button button;
-    private Uri humanUri, cameraUri;
+    private Uri humanUri, camaraUri;
     private String pathImageString, nameImageString;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,59 +34,70 @@ public class NewRegisterActivity extends AppCompatActivity implements View.OnCli
         //Controller
         controller();
 
-    }   //Main Method
+    }   // Main Method
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         //For Human
-        if ((requestCode == 0)&&(resultCode == RESULT_OK)) {
+        if ((requestCode == 0) && (resultCode == RESULT_OK)) {
             Log.d("24MayV1", "Human OK");
 
             //Show Image
             humanUri = data.getData();
             try {
 
-                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(humanUri));
-                humanImageView.setImageBitmap(bitmap);
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                            .openInputStream(humanUri));
+                    humanImageView.setImageBitmap(bitmap);
 
-                findPathAnName(humanUri);
+                    findPathAnName(humanUri);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
             } catch (Exception e) {
-                Log.d("24MaV1", "a humanUri ==>" + e.toString());
+                Log.d("24MayV1", "e humanUri ==> " + e.toString());
             }
 
-        } // if human
+
+        }   // if human
 
         // For Camera
-        if ((requestCode == 1)&&(resultCode == RESULT_OK)) {
+        if ((requestCode == 1) && (resultCode == RESULT_OK)) {
 
             Log.d("24MayV1", "Camera Result OK");
 
             //Show Image
-            cameraUri = data.getData();
+            camaraUri = data.getData();
             try {
 
-                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(cameraUri));
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                        .openInputStream(camaraUri));
                 humanImageView.setImageBitmap(bitmap);
 
-                findPathAnName(cameraUri);
+                findPathAnName(camaraUri);
 
             } catch (Exception e) {
-                Log.d("24MayV1", "e camera ==>" + e.toString());
+                Log.d("24MayV1", "e camera ==> " + e.toString());
             }
 
-        } //if camera
+
+        }   // if Camera
 
 
-    } //onActivity
+    }   // onActivity
 
     private void findPathAnName(Uri uri) {
+
         String[] strings = new String[]{MediaStore.Images.Media.DATA};
         Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
 
         if (cursor != null) {
+
             cursor.moveToFirst();
             int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             pathImageString = cursor.getString(index);
@@ -97,9 +105,9 @@ public class NewRegisterActivity extends AppCompatActivity implements View.OnCli
         } else {
             pathImageString = uri.getPath();
         }
-        Log.d("24MayV1", "Path ==>" + pathImageString);
-    }
+        Log.d("24MayV1", "Path ==> " + pathImageString);
 
+    }
 
     private void controller() {
         backImageView.setOnClickListener(this);
@@ -121,25 +129,25 @@ public class NewRegisterActivity extends AppCompatActivity implements View.OnCli
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
         //For Back
-        if (v == backImageView) {
+        if (view == backImageView) {
             finish();
         }
 
         //For Human
-        if (v == humanImageView) {
+        if (view == humanImageView) {
             Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("Image/*");
+            intent.setType("image/*");
             startActivityForResult(Intent.createChooser(intent, "Please Choose App for Choose Image"), 0);
         }
 
         //For Camera
-        if (v == cameraImageView) {
+        if (view == cameraImageView) {
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(intent, 1);
         }
 
     }
-}   //Main Class
+}   // Main Class
