e959247caebbd7a2617d0990fbd42d58e70fd6c1
cctvAndwifimap <peem1606@gmail.com>
2017-05-26 13:18:18 +0700

    26may

--- app/src/main/java/pkru/thongkam/sittichai/mypkru/ServiceActivity.java
+++ app/src/main/java/pkru/thongkam/sittichai/mypkru/ServiceActivity.java
@@ -1,5 +1,9 @@
 package pkru.thongkam.sittichai.mypkru;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +18,10 @@ public class ServiceActivity extends AppCompatActivity {
     private TextView textView;
     private ListView listView;
     private String[] loginStrings;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double latADouble = 7.909667, lngADouble = 98.387601;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,42 @@ public class ServiceActivity extends AppCompatActivity {
         createListView();
 
     }   //Main Method
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+        }
+
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            latADouble = location.getLatitude();
+            lngADouble = location.getLongitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     private void showName() {
 
@@ -63,6 +107,8 @@ public class ServiceActivity extends AppCompatActivity {
 
             }   //for
 
+
+
             FriendAdapter friendAdapter = new FriendAdapter(this, nameStrings, imageStrings);
             listView.setAdapter(friendAdapter);
 
@@ -75,6 +121,13 @@ public class ServiceActivity extends AppCompatActivity {
     private void initialView() {
         textView = (TextView) findViewById(R.id.txtNameLogin);
         listView = (ListView) findViewById(R.id.livFriend);
+
+        //Setup
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
     }
 
 
